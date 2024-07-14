3083a2d2350e993b5ef369f92fdc4ed2d4ae56d1
Brehima-traore <sqaad88@gmail.com>
2024-07-14 20:08:11 +0300

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,16 @@
+#list_work
+my_list =["pineaple","mango","orange","peach"]
+print(my_list)
+print(my_list[0:4:3])
+my_list.append("coconut")
+print(my_list)
+print(my_list[2:5])
+my_list[2]= "apple"
+print(my_list)
+#dict_work
+my_dict = {"hello":"привет","water":"вода","food":"еда"}
+print(my_dict)
+print(my_dict.get("hello"))
+my_dict["hello"]= "salut"
+my_dict["good bye"]= "пока"
+print(my_dict)
